814e2164d33f9b024a3568ae83889baee0779f3d
Tom Coleman <tom@thesnail.org>
2012-01-18 15:07:37 +1100

    Fixed a bug in the responder.

--- lib/edifice-forms/responder.rb
+++ lib/edifice-forms/responder.rb
@@ -4,14 +4,14 @@ module Edifice
     class Responder < ActionController::Responder
     protected 
       # add the :u_e header to xhr error requests
-      def to_html
+      def navigation_behavior(error)
         if controller.request.xhr? && !get? and has_errors? && default_action
           render :action => default_action, :status => :unprocessable_entity, :layout => nil
         else
-          super
+          super(error)
         end
       end
-    
+      
       # actually render something on successful updates
       def to_format
         unless get? or has_errors? or post?
